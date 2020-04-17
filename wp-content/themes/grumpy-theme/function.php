b94531dfce9daac9f12b45e419e4a15efc044777
alexszeliga <alexszeliga@gmail.com>
2020-04-17 14:05:16 -0400

    add style enqueue

--- wp-content/themes/grumpy-theme/function.php
+++ wp-content/themes/grumpy-theme/function.php
@@ -2,10 +2,8 @@
 
 
 function theme_stylesheet() {
-    if (!is_admin()) {
-        wp_register_style('theme-stylesheet', get_stylesheet_uri());
-        wp_enqueue_style('theme-stylesheet');
-    }
+        wp_register_style('style', get_stylesheet_uri());
+        wp_enqueue_style('style');
 }
 
 add_action( 'wp_enqueue_scripts', 'theme_stylesheet' ); // Add Theme Stylesheet
